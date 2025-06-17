3750758e40eabb04788c3a929c3138d0bc162f6f
kevin000006 <kevin.marinos@hotmail.com>
2025-06-17 14:02:23 -0500

    modificacion respuesta prueba

--- src/routes/actualizar-datos.routes.js
+++ src/routes/actualizar-datos.routes.js
@@ -4,7 +4,18 @@ const router = Router();
 
 router.get('/v1/data-contact',(req, res)=>{
 
-    res.json({});
+    res.json({
+        "userId": "string",
+        "contract": "string",
+        "companyId": "string",
+        "role": 4,
+        "userName": "string",
+        "companyName": "string",
+        "phoneNumber": "string",
+        "email": "string",
+        "isMultiCompany": true,
+        "isPlard": true 
+    });
 
 });
 
