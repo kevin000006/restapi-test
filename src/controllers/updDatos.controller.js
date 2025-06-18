f86cfb41cbd82db39c826853844f9ebd95df7ea0
kevin000006 <kevin.marinos@hotmail.com>
2025-06-18 02:00:21 -0500

    metodos autmento cupo

--- src/controllers/updDatos.controller.js
+++ src/controllers/updDatos.controller.js
@@ -15,3 +15,7 @@ export const getContextSesion=(req, res)=>{
         "isPlard": true 
     });
 };
+
+
+
+
--- src/routes/aumento-cupo.routes.js
+++ src/routes/aumento-cupo.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import {getFlujoPaso} from '../controllers/aumento-cupo.controller.js';
+import {getFlujoPaso,getDataIinitContext, getDocumentRASE,getDataConfirmacion} from '../controllers/aumento-cupo.controller.js';
 
 const router = Router();
 
@@ -9,24 +9,9 @@ const router = Router();
 router.get('/Customer_Offer/Card_Limit_Increase_Request/v1/campaigns/:id',async (req,res)=>{
     const [response] = await getFlujoPaso(1);
 
-    const dataResult ={
-        userId: "00756525689",
-        contract: "000000000000411",
-        companyId: "00713685691",
-        role: 4,
-        userName: "Diego Arellano Reyes",
-        companyName: "Servicio Agricola Ltda.",
-        cardholderName: "**** **** **** 0617",
-        cardUserName: "Diego A. Reyes",
-        cardNumber: "1234 4321 5698 0617",
-        currentQuota: "300000",
-        minimumQuota: "100000",
-        maximumQuota: "500000"
-    };
-
      setTimeout(() => {
         if(response.esExitoso == 1){
-                res.status(200).json(dataResult);
+                res.status(200).json(getDataIinitContext);
         }else{
             res.status(500).json({
                 code: 500,
@@ -38,5 +23,68 @@ router.get('/Customer_Offer/Card_Limit_Increase_Request/v1/campaigns/:id',async 
     },response.timeOut);
     
 });
+
+router.post('/Customer_Offer/Card_Limit_Increase_Request/v1/document-rase', async(req,res)=>{
+ 
+     const [response] = await getFlujoPaso(2);
+
+     setTimeout(() => {
+        if(response.esExitoso == 1){
+                res.status(200).json({
+                    document: getDocumentRASE.documento
+                });
+        }else{
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error",
+                level: "Error",
+                description: "An unexpected error occurred oan the server."
+            });
+        }
+    },response.timeOut);
+
+});
+
+router.post('/Customer_Offer/Card_Limit_Increase_Request/v1/generate-challenge', async(req,res)=>{
+
+     const [response] = await getFlujoPaso(3);
+
+     setTimeout(() => {
+        if(response.esExitoso == 1){
+                res.status(200).json({
+                    challenge: "desafio activo"
+                });
+        }else{
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error",
+                level: "Error",
+                description: "An unexpected error occurred oan the server."
+            });
+        }
+    },response.timeOut);
+
+});
+
+router.post('/Customer_Offer/Card_Limit_Increase_Request/v1/validate-challenge', async(req,res)=>{
+
+     const [response] = await getFlujoPaso(4);
+
+     setTimeout(() => {
+        if(response.esExitoso == 1){
+                res.status(200).json(getDataConfirmacion);
+        }else{
+            res.status(500).json({
+                code: 500,
+                message: "Internal Server Error",
+                level: "Error",
+                description: "An unexpected error occurred oan the server."
+            });
+        }
+    },response.timeOut);
+
+});
+
+
 
 export default router;
