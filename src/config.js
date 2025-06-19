46fff6f2f5c43a595f19a663aa837336d3626e5d
Kevin Marinos <kevin.marinos@hotmail.com>
2025-06-19 14:02:32 -0500

    cambio port

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const DB_PORT = process.env.DB_PORT || 3306;
 //Prod
 
 
-//export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "yamanote.proxy.rlwy.net";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "woQpftELRhoMfLONdhdwMylmzAlxxSyz";
