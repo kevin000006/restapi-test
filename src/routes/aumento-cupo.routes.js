bc05cca3936dc0b892076b0fa071a9b239934c25
kevin000006 <kevin.marinos@hotmail.com>
2025-09-26 00:11:35 -0500

    Cambio Servicio validacion desafio

--- src/routes/aumento-cupo.routes.js
+++ src/routes/aumento-cupo.routes.js
@@ -68,15 +68,36 @@ router.post('/Customer_Offer/Card_Limit_Increase_Request/v1/validate-challenge',
      const [response] = await getFlujoPaso(4);
 
      setTimeout(() => {
-        if(response.esExitoso == 1){
-                res.status(200).json(response.resultado);
-        }else{
-            res.status(500).json({
+        if(response.esExitoso == 0){
+             res.status(500).json({
                 code: 500,
                 message: "Internal Server Error",
                 level: "Error",
                 description: "An unexpected error occurred oan the server."
             });
+        }else{
+            let jsonRespuesta = {};
+            let ValidateStatus = '';
+
+            if(response.esExitoso == 1){
+                console.log(response.resultado);
+                response.resultado['documentVoucher'] = getDocumentRASE.documento;
+                jsonRespuesta = response.resultado;
+
+                ValidateStatus = 'CONFIRM';
+                res.status(200);
+            }else if(response.esExitoso == 2) // CANCELED_CHALLENGE
+            {
+                ValidateStatus = 'CANCELED_CHALLENGE';
+                res.status(204);
+            }
+            else if(response.esExitoso == 3) //NOT_AVAILABLE_YET
+            {
+                ValidateStatus = 'NOT_AVAILABLE_YET';
+                res.status(204);
+            }
+            res.set('ValidateStatus',ValidateStatus);
+            res.json(jsonRespuesta);
         }
     },response.timeOut);
 
